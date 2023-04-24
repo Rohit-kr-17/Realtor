8623c76d66ec682f8b1c545c0e10b38b6726ac3e
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2023-04-24 14:58:03 +0530

    Added map to the listing page using leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -21,6 +21,7 @@ import {
 } from "react-icons/fa";
 import { getAuth } from "firebase/auth";
 import Contact from "../Components/Contact";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Listing() {
 	const auth = getAuth();
@@ -150,7 +151,26 @@ function Listing() {
 						<Contact userRef={listing.userRef} listing={listing} />
 					)}
 				</div>
-				<div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+				<div className="w-full h-[200px] lg:h-[400px] z-10 overflow-x-hidden md:ml-2">
+					<MapContainer
+						center={[listing.geolocation.lat, listing.geolocation.lng]}
+						zoom={13}
+						scrollWheelZoom={false}
+						style={{ height: "100%", width: "100%" }}
+					>
+						<TileLayer
+							attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+							url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+						/>
+						<Marker
+							position={[listing.geolocation.lat, listing.geolocation.lng]}
+						>
+							<Popup>
+								A pretty CSS3 popup. <br /> Easily customizable.
+							</Popup>
+						</Marker>
+					</MapContainer>
+				</div>
 			</div>
 		</main>
 	);
